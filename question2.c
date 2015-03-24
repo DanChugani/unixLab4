4a13eabefc41c6ca41942785003a804febed1f59
DanChugani <chandanchugani@gmail.com>
2015-03-24 15:01:48 -0400

    fixed an error with writing to a file

--- question2.c
+++ question2.c
@@ -9,20 +9,21 @@ int i;
 struct group *groupName;
 struct passwd *pwdRec;
 char ** p;
-char name;
 FILE *f;
+
+ f = fopen("users.txt","w");
+	if (f==NULL)
+	{
+	 fprintf(stdout,"no file, you can start crying now...");
+	}
 
    if((groupName=getgrnam(argv[1])) != NULL)
    {      
       fprintf(stdout,"Group Name: %s\n",groupName->gr_name);
       	for( p = groupName->gr_mem; *p != NULL; p++)
-	{
-       		f = fopen("users.txt","w");
-			if (f==NULL){
-			 fprintf(stdout,"yolo");
-			}
-		fprintf(f, "\t%s\n", *p );
-    		fprintf(stdout,"\t%s\n",*p);
+	{     	
+	 fprintf(f, "\t%s\n", *p );
+    	 fprintf(stdout,"\tmember:%s\n",*p);
 	}
 
 
