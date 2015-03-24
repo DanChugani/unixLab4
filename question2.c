a2452078805d4b2b490c7e370b98a2954b8e692c
DanChugani <chandanchugani@gmail.com>
2015-03-24 17:10:55 -0400

    all the core features have been laid down.

--- question2.c
+++ question2.c
@@ -5,32 +5,32 @@
 int main(int argc, char *argv[],char * envp[])
 {
 
-int i;
-struct group *groupName;
+struct group *groupName; 
 struct passwd *pwdRec;
 char ** p;
 FILE *f;
-
+ /*Open/make a file called user.txt */
  f = fopen("users.txt","w");
-	if (f==NULL)
+	if (f==NULL) /*check and make sure file is empty*/
 	{
 	 fprintf(stdout,"no file, you can start crying now...");
 	}
 
-   if((groupName=getgrnam(argv[1])) != NULL)
+   if((groupName=getgrnam(argv[1])) != NULL)/*Checks and makes sure that argv[1] is in /etc/group */
    {      
-      fprintf(stdout,"Group Name: %s\n",groupName->gr_name);
-      	for( p = groupName->gr_mem; *p != NULL; p++)
+    fprintf(stdout,"Group Name: %s\n",groupName->gr_name);
+      	for( p = groupName->gr_mem; *p != NULL; p++)/*Check if group has members*/
 	{     	
-	 fprintf(f, "\t%s\n", *p );
-    	 fprintf(stdout,"\tmember:%s\n",*p);
+	//fprintf(f, "\t%s\n", *p );
+    	 fprintf(stdout,"\tmember:%s\n",*p); /*print out group member(s)*/
+	 
+	 pwdRec=getpwnam(*p);/*set pwdRec to the value of *p(Group Name)*/
+         putpwent(pwdRec,f);/*Write all the information to the file*/
 	}
-
-
-   }else
+   }else/*If group Name does not exist in /etc/group*/
 	{
 		fprintf(stdout,"Group Name: \"%s\" was not found\n",argv[1]);
 	}
-
-}
+ fclose(f); /*Always close your files*/
+}/*end of main*/
 
