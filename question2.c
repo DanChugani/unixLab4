6ed10f0d232268b94d6391ed270713edfe0a564c
DanChugani <chandanchugani@gmail.com>
2015-03-24 14:07:30 -0400

    modified reading now writes to a file named users.txt

--- question2.c
+++ question2.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <grp.h>
+#include <pwd.h>
 
-int main(int argc, char * argv[ ])
+int main(int argc, char *argv[],char * envp[])
 {
+
 int i;
 struct group *groupName;
+struct passwd *pwdRec;
 char ** p;
-
+char name;
+FILE *f;
 
    if((groupName=getgrnam(argv[1])) != NULL)
    {      
       fprintf(stdout,"Group Name: %s\n",groupName->gr_name);
-      for( p = groupName->gr_mem; *p != NULL; p++ ) {
-       printf( "\t%s\n", *p );
-    	}
-      // fprintf(stdout,"GID: %d\n",groupName->gr_gid);
-      /*
-	//List everyone in the group
-         for(i=0; groupRec->gr_mem[i]; i++)
-            fprintf(stdout,"   member: %s\n",groupRec->gr_mem[i]);
-            fprintf(stdout,"\n\n");
-	*/
-   }
+      	for( p = groupName->gr_mem; *p != NULL; p++)
+	{
+       		f = fopen("users.txt","w");
+			if (f==NULL){
+			 fprintf(stdout,"yolo");
+			}
+		fprintf(f, "\t%s\n", *p );
+    		fprintf(stdout,"\t%s\n",*p);
+	}
+
+
+   }else
+	{
+		fprintf(stdout,"Group Name: \"%s\" was not found\n",argv[1]);
+	}
 
 }
 
